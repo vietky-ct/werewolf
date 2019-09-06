531704f06d017114c7c7afc457b861f303f98769
danztran <ntctheboy97@gmail.com>
2019-09-06 13:42:33 +0700

    add combinding images into one

---
+++ assets/combine.py
@@ -0,0 +1,42 @@
+import sys
+import math
+from PIL import Image
+
+from os import walk
+
+MAX_COLUMNS = 6
+COMBINED_FILE = "sprites.jpg"
+IMAGES_PATH = "./images/"
+
+def get_filepaths_in_dir(dirpath):
+  filepaths = []
+  for (dirpath, dirnames, filenames) in walk(dirpath):
+    for name in filenames:
+      filepaths.append(dirpath + name)
+    break
+  return filepaths
+
+def combine_images(save_path, image_paths, max_columns):
+  images = map(Image.open, image_paths)
+  widths, heights = zip(*(i.size for i in images))
+
+  max_width = max(widths)
+  max_height = max(heights)
+  total_width = max_width * max_columns
+  total_height = max_height * math.ceil(len(image_paths) / max_columns)
+
+  new_im = Image.new('RGB', (total_width, total_height))
+
+  x_offset = 0
+  y_offset = 0
+  for index, image in enumerate(image_paths):
+    img = Image.open(image)
+    new_im.paste(img, (x_offset, y_offset))
+    x_offset += max_width
+    if (index + 1) % max_columns == 0:
+      y_offset += max_height
+      x_offset = 0
+
+  new_im.save(save_path)
+
+combine_images(COMBINED_FILE, get_filepaths_in_dir(IMAGES_PATH), MAX_COLUMNS)
