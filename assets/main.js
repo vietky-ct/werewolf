c329fd45fe9d42228dc18b395ac24e8a2b2df2c1
quangle <quangle@chotot.vn>
2019-09-26 09:59:47 +0700

    update get all

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,51 @@
-const data = require('./data.json');
+$.get({
+    url: "http://localhost:3000/cards/get-all",
+    success: function (data) {
+        let imgDiv = document.querySelector(".cards__image");
+        let titleDiv = document.querySelector(".cards__title");
+        let scoreDiv = document.querySelector(".cards__point");
+        let checkboxDiv = document.querySelector(".cards__checkbox");
 
-function loadData() {
-    let imgDiv = document.querySelector(".cards__image");
-    let titleDiv = document.querySelector(".cards__title");
-    let scoreDiv = document.querySelector(".cards__point");
-    let checkboxDiv = document.querySelector(".cards__checkbox");
+        data.forEach((data) => {
+            let x = document.createElement('IMG');
+            let y = document.createElement('P');
+            let z = document.createElement('P');
+            let t = document.createElement('INPUT');
 
-    data.forEach((data) => {
-        let x = document.createElement('IMG');
-        let y = document.createElement('P');
-        let z = document.createElement('P');
-        let t = document.createElement('INPUT');
+            x.setAttribute('src', data.src);
+            x.setAttribute('class', 'role');
+            t.setAttribute('type', 'checkbox');
+            t.setAttribute('value', data.id);
+            t.setAttribute('name', 'roleChosed');
 
-        x.setAttribute('src', data.src);
-        x.setAttribute('class', 'role');
-        t.setAttribute('type', 'checkbox');
-        
-        y.innerText = data.name;
+            y.innerText = data.name;
 
-        z.innerText = data.score;
+            z.innerText = data.score;
 
-        imgDiv.appendChild(x);
-        titleDiv.appendChild(y);
-        scoreDiv.appendChild(z);
-        checkboxDiv.appendChild(t);
-    })
-}
+            imgDiv.appendChild(x);
+            titleDiv.appendChild(y);
+            scoreDiv.appendChild(z);
+            checkboxDiv.appendChild(t);
+        })
+    }
+});
 
-loadData()
+$("#btn__next").on('click', () => {
+    let result = {};
+    result.total_player = 0;
+    result.selected_characters = [];
+    var numbersOfPlayer = $('#players__input').val();
+    $('input[name="roleChosed"]:checked').each(function () {
+        result.selected_characters.push(this.value);
+    });
+    result.total_player = numbersOfPlayer;
+    console.log(result.selected_characters);
+    $.post('http://localhost:3000/cards/init',
+        {
+            'total_player': result.total_player,
+            'selected_characters[]': result.selected_characters
+        }, (data) => {
+            console.log('My data: ', data)
+        }
+    );
+})
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 3000
 const path = require('path')
 const bodyParser = require('body-parser')
 const {
@@ -8,27 +8,36 @@ const {
     initGame,
 } = require('./handlers');
 
+
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: false
 }));
 app.use(express.static('assets'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.post('/cards/init', (req, res) => {
     console.log('req', req.body, req.params)
 
     let totalPlayer = req.body.total_player;
     let selectedCharacters = req.body.selected_characters;
-    let patterns = initGame(totalPlayer, selectedCharacters);
+    console.log('Total players: ', totalPlayer);
+    console.log(selectedCharacters);
+    // let patterns = initGame(totalPlayer, selectedCharacters);
 
-    res.status(200)
-        .json(patterns).end()
+    // res.status(200)
+        // .json(selectedCharacters).end()
 })
 
-// app.get('/cards/get-all', (req, res) => {
-//     res.json(characters)
-//         .end()
-// })
+app.get('/cards/get-all', (req, res) => {
+    res.json(characters)
+        .end()
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/assets/home.html'));
